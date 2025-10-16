db1e0cbe3ae4dd2e9cade147ced76fca7894379d
nastiachaban <cabananastasia51@gmail.com>
2025-10-16 16:18:44 +0200

    navbar changed

--- src/main/java/com/example/school/repos/StudentRepo.java
+++ src/main/java/com/example/school/repos/StudentRepo.java
@@ -3,5 +3,8 @@ package com.example.school.repos;
 import com.example.school.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepo extends JpaRepository<Student, Long> {
+    Optional<Student> getStudentByAccountId(Long AccountId);
 }
--- src/main/java/com/example/school/services/AccountService.java
+++ src/main/java/com/example/school/services/AccountService.java
@@ -1,9 +1,12 @@
 package com.example.school.services;
 
 import com.example.school.entities.Account;
+import com.example.school.entities.Picture;
 import com.example.school.entities.SignUp;
+import com.example.school.entities.Student;
 import com.example.school.entities.enums.UserType;
 import com.example.school.repos.AccountRepo;
+import com.example.school.repos.StudentRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class AccountService {
@@ -19,14 +23,17 @@ public class AccountService {
     @Autowired
     private PasswordEncoder passwordEncoder;
     @Autowired
-    private AccountRepo repo;
+    private AccountRepo accountRepo;
+
+    @Autowired
+    private StudentRepo studentRepo;
 
     private final Logger LOGGER= LoggerFactory.getLogger(AccountService.class);
 
     @Transactional
     public void signup(SignUp request) {
         String username = request.getUsername();
-        Optional<Account> existingUser = repo.findByUsername(username);
+        Optional<Account> existingUser = accountRepo.findByUsername(username);
         if (existingUser.isPresent()) {
             throw new RuntimeException(String.format("Account with the same username '%s' already exists.", username));
         }
@@ -36,15 +43,18 @@ public class AccountService {
         }
 
         String hashedPassword = passwordEncoder.encode(request.getPassword());
-        Account account= new Account(request.getUsername(),request.getEmail(),hashedPassword,UserType.STUDENT);
-//        Account account = Account.builder()
-//                .username(request.getUsername())
-//                .email(request.getEmail())
-//                .password(hashedPassword)
-//                .userType(UserType.STUDENT)
-//                .build();
+       // Account account= new Account((new Random()).nextLong(),new Picture(), request.getUsername(),request.getEmail(),hashedPassword,UserType.STUDENT);
+        Account account = Account.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(hashedPassword)
+                .userType(UserType.STUDENT)
+                .build();
 
-        repo.save(account);
+        accountRepo.save(account);
+
+        Student student= Student.builder().account(account).build();
+        studentRepo.save(student);
     }
 
 }
